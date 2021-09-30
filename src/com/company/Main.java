8d4461b18447f757e5f42f14b77bea2029f07e87
stinja <20714232+stinja@users.noreply.github.com>
2021-09-29 22:37:24 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String myString1 = "this is a string.";
+        String myString2 = "this is string 2 and has an int.";
+        String myString3 = "this is string 3 and has an int 13.4 and double.";
+        String myString4 = "this, is, a, string, with, some, delimiters";
+        String myString5 = "this\nis\na\nstring\nthat\nuses\nln\nas\ndelimiter";
+    }
+}
